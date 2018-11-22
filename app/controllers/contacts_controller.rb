21f241aea86a97ed2a6bcba92fe266fb033fe96c
ramonsantos <ramonsantos.pe@gmail.com>
2018-11-22 18:48:44 -0300

    Add 'Address' model

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,14 +9,14 @@ class ContactsController < ApplicationController
 
   # GET /contacts/1
   def show
-    render json: @contact, include: [:kind, :phones]
+    render json: @contact, include: [:kind, :phones, :address]
   end
 
   # POST /contacts
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
-      render json: @contact, include: [:kind, :phones],  status: :created, location: @contact
+      render json: @contact, include: [:kind, :phones, :address],  status: :created, location: @contact
     else
       render json: @contact.errors, status: :unprocessable_entity
     end
@@ -25,7 +25,7 @@ class ContactsController < ApplicationController
   # PATCH/PUT /contacts/1
   def update
     if @contact.update(contact_params)
-      render json: @contact, include: [:kind, :phones]
+      render json: @contact, include: [:kind, :phones, :address]
     else
       render json: @contact.errors, status: :unprocessable_entity
     end
@@ -45,7 +45,8 @@ class ContactsController < ApplicationController
   def contact_params
     params.require(:contact).permit(
       :name, :email, :birthdate, :kind_id,
-      phones_attributes: [:id, :number, :_destroy]
+      phones_attributes: [:id, :number, :_destroy],
+      address_attributes: [:id, :street, :city]
     )
   end
 end
--- lib/tasks/dev.rake
+++ lib/tasks/dev.rake
@@ -2,6 +2,9 @@ namespace :dev do
   desc 'Configura o ambiente de desenvolvimento'
 
   task setup: :environment do
+    puts "Resetando o banco de dados..."
+    %x(rails db:drop db:create db:migrate)
+
     puts "Cadastrando os tipos de contato..."
     kinds = %w(Amigo Comercial Conhecido)
     kinds.each do |kind|
@@ -32,5 +35,17 @@ namespace :dev do
     end
 
     puts 'Telefones cadastrados com sucesso!'
+
+    ######################
+
+    puts "Cadastrando os endereços..."
+    Contact.all.each do |contact|
+      Address.create(
+        street: Faker::Address.street_address,
+        city: Faker::Address.city,
+        contact: contact
+      )
+    end
+    puts "Endereços cadastrados com sucesso!"
   end
 end
