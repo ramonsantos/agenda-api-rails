3b6edd66a70fac4b2409c9fa7ea70182897b972c
ramonsantos <ramonsantos.pe@gmail.com>
2018-11-26 18:52:52 -0300

    Configures 'Media Types'

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,7 +9,7 @@ class ContactsController < ApplicationController
 
   # GET /contacts/1
   def show
-    render json: @contact, include: [:kind] #, meta: { author: "Jackson Pires" }   #, include: [:kind, :phones, :address]
+    rrender json: @contact, include: [:kind, :address, :phones] #, meta: { author: "Jackson Pires" }   #, include: [:kind, :phones, :address]
   end
 
   # POST /contacts
